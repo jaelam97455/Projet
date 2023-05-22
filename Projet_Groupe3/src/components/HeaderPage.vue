e8a29595fe933fe2bb1041f8dd41d06aa03b4a21
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-05-22 12:32:53 +0200

    ajout des assets

--- Projet_Groupe3/src/components/HeaderPage.vue
+++ Projet_Groupe3/src/components/HeaderPage.vue
@@ -2,47 +2,4 @@
 </script>
 
 <template>
-  <header aria-label="Site Header" class="bg-white">
-    <div class="flex items-center h-16 max-w-screen-xl gap-8 px-4 mx-auto sm:px-6 lg:px-8">
-      <a class="flex items-center justify-center w-12 h-12 text-sm text-white bg-indigo-600 rounded-full" href="/">Logo</a>
-
-      <div class="flex items-center justify-end flex-1 md:justify-between">
-        <nav aria-label="Site Nav" class="hidden md:block">
-          <ul class="flex items-center gap-6 text-sm text-gray-500">
-            <li>
-              <a class="transition hover:text-gray-700" href="/"> About </a>
-            </li>
-            <li>
-              <a class="transition hover:text-gray-700" href="/"> Careers </a>
-            </li>
-            <li>
-              <a class="transition hover:text-gray-700" href="/"> History </a>
-            </li>
-            <li>
-              <a class="transition hover:text-gray-700" href="/"> Services </a>
-            </li>
-            <li>
-              <a class="transition hover:text-gray-700" href="/"> Projects </a>
-            </li>
-          </ul>
-        </nav>
-
-        <div class="flex items-center gap-4">
-          <div class="sm:flex sm:gap-4">
-            <a class="block rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-indigo-700" href="/"> Login </a>
-
-            <a class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-indigo-600 transition hover:text-indigo-600/75 sm:block" href="/"> Register </a>
-          </div>
-
-          <button class="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
-            <span class="sr-only">Toggle menu</span>
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </div>
-  </header>
-
 </template>
--- Projet_Groupe3/tailwind.config.js
+++ Projet_Groupe3/tailwind.config.js
@@ -4,14 +4,14 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      indigo: '#1d096b',
-      pink: '#feaa9b',
-      white: '#ffffff',
-      black: '#000000',
-      transparent: 'transparent',
-      gray: colors.zinc
-    },
+    // colors: {
+    //   indigo: '#1d096b',
+    //   pink: '#feaa9b',
+    //   white: '#ffffff',
+    //   black: '#000000',
+    //   transparent: 'transparent',
+    //   gray: colors.zinc
+    // },
     fontFamily: {
       sans: ['Mulish', 'sans-serif'],
       serif: ['Space Mono', 'monospace']
