bbdbd21651cb283147c6d504744d9bfb3cc2b95d
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-05-28 14:38:53 +0200

    fix hero section adapation md

--- Projet_Groupe3/src/components/Hero.vue
+++ Projet_Groupe3/src/components/Hero.vue
@@ -11,10 +11,10 @@ defineProps<{
     <div class="relative">
         <img :src="imgPath" :alt="imgAlt" class="w-full h-auto">
 
-        <div class="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full">
-            <h1 class="px-5 text-5xl font-bold text-center text-darkgreen">{{ title }}</h1>
+        <div class="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full gap-[4vw]">
+            <h1 class="text-5xl font-bold text-center text-darkgreen lg:text-7xl md:text-6xl mx-[8vw]">{{ title }}</h1>
             <RouterLink to="/"
-                class="px-4 py-2 text-white mt-14 rounded-xl hover:bg-lightgreen hover:text-white active:border-darkgreen active:border-4 bg-orange">
+                class="px-4 py-2 text-white mt-14 lg:mt-0 rounded-xl hover:bg-lightgreen hover:text-white active:border-darkgreen active:border-4 bg-orange lg:text-xl">
                 FAIRE MES COURSES</RouterLink>
         </div>
     </div>
--- Projet_Groupe3/src/pages/index.vue
+++ Projet_Groupe3/src/pages/index.vue
@@ -5,18 +5,18 @@ import CagetteCard from '@/components/CagetteCard.vue';
 <template>
     <Hero title="Prenez goût à l'anti-gaspi, 
     faites des économies ! " imgPath="../../public/img/hero-pic-mobile.webp" imgAlt="Fruits et légumes"
-        class="lg:hidden " />
+        class="md:hidden " />
     <Hero title="Prenez goût à l'anti-gaspi, 
     faites des économies ! " imgPath="../../public/img/hero-pic.webp" imgAlt="Fruits et légumes"
-        class="hidden lg:block " />
-        <div class="flex flex-wrap justify-center">
-    <CagetteCard title="Abonnement Light" imgPath="../../public/img/cagette-1.webp"
-        imgAlt="Cagette remplie de fruits et légumes" price="20 €" name1="1kg | Pomme de terre" name2="0.2kg | Ail"
-        name3="0.5kg | Abricots" name4="1kg | Tomates" name5="0.5kg | Fraises" name6="1kg | Pêches" name7="0.2kg | Noix"
-        name8="1kg | Aubergines" name9="0.2kg | Framboises" />
-    <CagetteCard title="Abonnement Giant" imgPath="../../public/img/cagette-3.webp"
-        imgAlt="Cagette remplie de fruits et légumes" price="40 €" name1="4kg | Pomme de terre" name2="4kg | Artichaut"
-        name3="1kg | Tomates" name4="2kg | Carottes" name5="3kg | Courgettes" name6="2kg | Poires" name7="0.5kg | Oignons"
-        name8="2kg | Pommes" name9="2kg | Concombres" />
-        </div>
+        class="hidden md:block " />
+    <div class="flex flex-wrap justify-center">
+        <CagetteCard title="Abonnement Light" imgPath="../../public/img/cagette-1.webp"
+            imgAlt="Cagette remplie de fruits et légumes" price="20 €" name1="1kg | Pomme de terre" name2="0.2kg | Ail"
+            name3="0.5kg | Abricots" name4="1kg | Tomates" name5="0.5kg | Fraises" name6="1kg | Pêches" name7="0.2kg | Noix"
+            name8="1kg | Aubergines" name9="0.2kg | Framboises" />
+        <CagetteCard title="Abonnement Giant" imgPath="../../public/img/cagette-3.webp"
+            imgAlt="Cagette remplie de fruits et légumes" price="40 €" name1="4kg | Pomme de terre" name2="4kg | Artichaut"
+            name3="1kg | Tomates" name4="2kg | Carottes" name5="3kg | Courgettes" name6="2kg | Poires"
+            name7="0.5kg | Oignons" name8="2kg | Pommes" name9="2kg | Concombres" />
+    </div>
 </template>
