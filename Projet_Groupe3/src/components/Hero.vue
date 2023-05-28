af2308236e4e17e6e8658e13d2bd56930cfd676e
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-05-28 17:49:54 +0200

    fin section don homepage

--- Projet_Groupe3/src/components/Hero.vue
+++ Projet_Groupe3/src/components/Hero.vue
@@ -15,7 +15,7 @@ defineProps<{
         <div class="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full gap-[4vw]">
             <h1 class="text-5xl font-bold text-center text-darkgreen lg:text-7xl md:text-6xl mx-[8vw] animate-fade-in">{{ title }}</h1>
             <RouterLink to="/"
-                class="px-4 py-2 text-white mt-14 lg:mt-0 rounded-xl hover:bg-lightgreen hover:text-white active:border-darkgreen active:border-4 bg-orange lg:text-xl">
+                class="px-4 py-2 text-white mt-14 lg:mt-0 rounded-xl hover:bg-lightgreen hover:text-white active:border-lightgreen active:border-4 bg-orange lg:text-xl">
                 FAIRE MES COURSES</RouterLink>
         </div>
     </div>
--- Projet_Groupe3/src/pages/index.vue
+++ Projet_Groupe3/src/pages/index.vue
@@ -13,7 +13,7 @@ import CardEtape from '@/components/CardEtape.vue';
         class="hidden md:block " />
     <section>
         <div class="flex flex-col justify-center gap-4 mx-4 my-8">
-            <img src="../../public/img/illu-ble.webp" alt="épis de blé" class="max-w-[54px] m-auto">
+            <img src="/img/illu-ble.webp" alt="épis de blé" class="max-w-[54px] m-auto">
             <h2 class="text-4xl font-bold text-center">Notre sélection de produits</h2>
             <p class="text-center">Découvrez nos abonnements pour recevoir chaque mois votre cagette de fruits et
                 légumes&nbsp;!</p>
@@ -30,12 +30,31 @@ import CardEtape from '@/components/CardEtape.vue';
                 name6="2kg | Poires" name7="0.5kg | Oignons" name8="2kg | Pommes" name9="2kg | Concombres" />
         </div>
     </section>
+
     <CounterBox />
-            <div class="flex flex-col justify-center gap-4 mx-4 my-8">
-                <img src="../../public/img/illu-panier.webp" alt="panier de légumes" class="max-w-[54px] m-auto">
-                <h2 class="text-4xl font-bold text-center">Composez votre cagette !</h2>
-                <p class="text-center">Légumes de saison, fruits rouges... Vous souhaitez un produit bien particulier&nbsp;?</p>
-            </div>
-            
-            <CardEtape/>
+
+    <section>
+        <div class="flex flex-col justify-center gap-4 mx-4 my-8">
+            <img src="/img/illu-panier.webp" alt="panier de légumes" class="max-w-[54px] m-auto">
+            <h2 class="text-4xl font-bold text-center">Composez votre cagette&nbsp;!</h2>
+            <p class="text-center">Légumes de saison, fruits rouges... Vous souhaitez un produit bien particulier&nbsp;?</p>
+        </div>
+
+        <CardEtape />
+    </section>
+
+    <div class="flex flex-col justify-center gap-4 mx-4 my-8">
+        <img src="/img/illu-eco.webp" alt="panier de légumes" class="max-w-[54px] m-auto">
+        <h2 class="text-4xl font-bold text-center">Sauver des produits&nbsp;?! <br>Comment ça marche&nbsp;?</h2>
+                <img src="/img/deco-poire-banane.webp" alt="panier de légumes" class="m-auto">
+        <p class="text-center">Vous avez un potager, des invendus, des produits en trop&nbsp;? <br>
+            Donnez-les pour bénéficier de nombreuses réductions&nbsp;!</p>
+                    <img src="/img/deco-avocat-poivron.webp" alt="panier de légumes" class="m-auto">
+                    <p class="text-center">Nous récupérons les produits trop <span class="uppercase text-orange">PETITS</span>, trop <span class="uppercase text-orange">TORDUS</span>, trop <span class="uppercase text-orange">DIFFORMES</span>.
+    </p>
+    <h3 class="text-3xl font-bold text-center text-lightgreen">Mais toujours aussi savoureux&nbsp;!</h3>
+                <RouterLink to="/"
+                    class="px-4 py-2 m-auto my-8 text-center text-white rounded-xl hover:bg-lightgreen hover:text-white active:border-lightgreen active:border-4 bg-orange lg:text-xl w-fit">
+                    FAIRE UN DON</RouterLink>
+    </div>
 </template>
