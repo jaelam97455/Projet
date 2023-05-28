e499a446704b4ebd454dc074df1b6865b60ddf05
Lisa Cingolani <lisa.cingolani@edu.univ-fcomte.fr>
2023-05-28 12:39:03 +0200

    fonctions colorant les boutons selon le nom

--- Projet_Groupe3/src/components/CagetteCard.vue
+++ Projet_Groupe3/src/components/CagetteCard.vue
@@ -5,58 +5,41 @@ defineProps<{
     imgPath: String
     imgAlt: String
     price: String
+    name1: String
+    name2: String
+    name3: String
+    name4: String
+    name5: String
+    name6: String
+    name7: String
+    name8: String
+    name9: String
 }>()
+
+function containsFruit(name: string): boolean {
+    const fruits = ['Pommes', 'Poires', 'Abricot', 'Tomates', 'Pêches', 'Fraises', 'Framboises', 'Noix'];
+    return fruits.some(fruit => name.toLowerCase().includes(fruit.toLowerCase()));
+}
 </script>
 
 <template>
     <div class="flex flex-col items-center overflow-hidden bg-white rounded-xl shadow-1 gap-4 m-[5vw]">
-        <div
-            class="relative flex flex-col items-center self-stretch justify-start overflow-hidden rounded-t-xl bg-darkgreen">
+        <div class="relative flex flex-col items-center self-stretch justify-start overflow-hidden rounded-t-xl bg-darkgreen"
+            :class="{ 'bg-darkgreen': title !== 'Abonnement Light', 'bg-orange': title === 'Abonnement Light' }">
             <p class="font-serif text-2xl font-bold text-center text-white">{{ title }}
             </p>
         </div>
-        <div class="relative flex flex-col items-start self-stretch justify-start overflow-hidden bg-white rounded-xl px-[2vh]">
+        <div
+            class="relative flex flex-col items-start self-stretch justify-start overflow-hidden bg-white rounded-xl px-[2vh]">
             <img :src="imgPath" :alt="imgAlt" class="self-stretch object-cover rounded-xl" />
         </div>
 
         <div class="flex flex-wrap items-center px-[2vh]">
-
-            <div class="relative flex items-center justify-center m-1 rounded-lg bg-darkgreen">
-                <p class="text-base text-center text-white btn-cagette-card">4kg | Pomme de terre</p>
+            <div v-for="name in [name1, name2, name3, name4, name5, name6, name7, name8, name9]" :key="name"
+                class="relative flex items-center justify-center m-1 rounded-lg"
+                :class="{ 'bg-darkgreen': !containsFruit(name), 'bg-orange': containsFruit(name) }">
+                <p class="text-base text-center text-white btn-cagette-card">{{ name }}</p>
             </div>
-
-            <div class="relative flex items-center justify-center m-1 rounded-lg bg-darkgreen">
-                <p class="text-base text-center text-white btn-cagette-card">4kg | Artichaut</p>
-            </div>
-
-            <div class="relative flex items-center justify-center m-1 rounded-lg bg-darkgreen">
-                <p class="text-base text-center text-white btn-cagette-card">1kg | Tomates</p>
-            </div>
-
-            <div class="relative flex items-center justify-center m-1 rounded-lg bg-darkgreen">
-                <p class="text-base text-center text-white btn-cagette-card">2kg | Carottes</p>
-            </div>
-
-            <div class="relative flex items-center justify-center m-1 rounded-lg bg-darkgreen">
-                <p class="text-base text-center text-white btn-cagette-card">3kg | Courgettes</p>
-            </div>
-
-            <div class="relative flex items-center justify-center m-1 rounded-lg bg-darkgreen">
-                <p class="text-base text-center text-white btn-cagette-card">2kg | Poires</p>
-            </div>
-
-            <div class="relative flex items-center justify-center m-1 rounded-lg bg-darkgreen">
-                <p class="text-base text-center text-white btn-cagette-card">0.5kg | Oignons</p>
-            </div>
-
-            <div class="relative flex items-center justify-center m-1 rounded-xl bg-darkgreen">
-                <p class="text-base text-center text-white btn-cagette-card">2kg | Pommes</p>
-            </div>
-
-            <div class="relative flex items-center justify-center m-1 rounded-xl bg-orange">
-                <p class="text-base text-center text-white btn-cagette-card">2kg | Concombres</p>
-            </div>
-
         </div>
 
         <div class="relative flex items-center self-stretch justify-between px-[3vh] mb-4">
